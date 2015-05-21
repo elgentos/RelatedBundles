6047ba154f0c4f118464ecaf4672c359f0dbb3af
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2015-05-21 14:05:22 +0200

    Moved files to correct dir

--- app/code/community/Elgentos/RelatedBundles/Block/Adminhtml/Catalog/Product/Edit/Tab.php
+++ app/code/community/Elgentos/RelatedBundles/Block/Adminhtml/Catalog/Product/Edit/Tab.php
@@ -8,7 +8,7 @@ implements Mage_Adminhtml_Block_Widget_Tab_Interface
     {
         /* Do not show the Related Bundles tab for bundled products itself */
         $product = Mage::registry('current_product');
-        if($product->getTypeId() == 'Mage_Catalog_Model_Product_Type::TYPE_BUNDLE') {
+        if($product->getTypeId() == Mage_Catalog_Model_Product_Type::TYPE_BUNDLE) {
             return false;
         }
         return (($this->getRequest()->getActionName() === 'new') && (!$this->getRequest()->getParam('set')))
